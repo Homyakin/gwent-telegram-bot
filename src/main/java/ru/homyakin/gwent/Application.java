69dc97d6b99b5e045a1ae19dd4b5527074b1b9f6
Homyakin <tox.home@ya.ru>
2020-07-30 19:57:18 +0300

    [NF] micronaut command line application

--- src/main/java/ru/homyakin/gwent/Application.java
+++ src/main/java/ru/homyakin/gwent/Application.java
@@ -1,7 +1,16 @@
 package ru.homyakin.gwent;
 
-public class Application {
-    public static void main(String... args) {
+import io.micronaut.configuration.picocli.PicocliRunner;
+import picocli.CommandLine;
 
+@CommandLine.Command(name = "gwent-telegram-bot")
+public class Application implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello, world");
+    }
+
+    public static void main(String... args) {
+        PicocliRunner.run(Application.class, args);
     }
 }
