d5951d3f1de928f4cd7e455c67122539543204fa
Homyakin <tox.home@ya.ru>
2020-08-08 19:20:35 +0300

    [NF] added docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 mvn package spring-boot:repackage &&
-docker build -f Dockerfile -t gwent-bot-img . &&
+docker build -f Dockerfile -t gwent-bot-img . || true &&
 docker stop gwent-bot || true &&
 docker rm gwent-bot || true &&
 docker run --name gwent-bot -d gwent-bot-img
